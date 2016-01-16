89f2505c9a092bb4c92bd63b2e7f91ddd7ee1d6d
Marko Markovic <fatdzo@gmail.com>
2016-01-16 15:51:16 -0500

    Added bill detail vote adapter and connected it to the view

--- app/src/main/java/com/tabsontally/markomarks/tabsontally/BillDetailActivity.java
+++ app/src/main/java/com/tabsontally/markomarks/tabsontally/BillDetailActivity.java
@@ -1,13 +1,43 @@
 package com.tabsontally.markomarks.tabsontally;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class BillDetailActivity extends AppCompatActivity {
+
+    private BillVoteAdapter billVoteAdapter;
+    private Context context;
+    ArrayList<BillVoteItem> billVoteItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bill_detail);
+
+        context = this;
+
+
+        initializeControls();
+
+
     }
+
+    private void initializeControls()
+    {
+        BillVoteItem temp = new BillVoteItem();
+        billVoteItems.add(temp);
+
+
+
+        billVoteAdapter = new BillVoteAdapter(context, billVoteItems);
+
+        ListView lstVoters =  (ListView) findViewById(R.id.lst_Voters);
+        lstVoters.setAdapter(billVoteAdapter);
+
+    }
+
 }
--- app/src/main/java/com/tabsontally/markomarks/tabsontally/BillVoteAdapter.java
+++ app/src/main/java/com/tabsontally/markomarks/tabsontally/BillVoteAdapter.java
@@ -1,11 +1,16 @@
 package com.tabsontally.markomarks.tabsontally;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -14,10 +19,15 @@ import java.util.ArrayList;
  */
 public class BillVoteAdapter extends ArrayAdapter<BillVoteItem> {
 
+    private Context ctx;
+
     public BillVoteAdapter(Context context, ArrayList<BillVoteItem> items)
     {
         super(context, 0, items);
+        ctx = context;
     }
+
+
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -27,6 +37,7 @@ public class BillVoteAdapter extends ArrayAdapter<BillVoteItem> {
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.bill_voter_item, parent, false);
         }
+
         // Lookup view for data population
         TextView billIndex = (TextView) convertView.findViewById(R.id.txt_BillDetailVoteIndex);
         billIndex.setText(String.valueOf(bill.Index) + ". ");
